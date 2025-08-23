8e6c7e6b62b194ffc5497da056bfbf658e1ebb2f
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-08-23 15:29:58 +0300

    Second commit, added two new tasks with name of class traingle pattern ad SumofThreeNumbers.

---
+++ Module-1-1/src/SumOfThreeNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SumOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("The sum of the numbers is " + (first + second + third));
+
+        int product = first * second * third;
+        System.out.println("The product of the numbers is " + product);
+
+        double average = (double) (first + second + third) /3;
+        System.out.println("The average of the numbers is " + average);
+    }
+}
+
