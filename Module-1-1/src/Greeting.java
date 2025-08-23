b71ea446dd7f8ced97ec8593713982d0319c2698
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-08-23 11:12:42 +0300

    First commit, created a module and write a first java program for with greeting class.

---
+++ Module-1-1/src/Greeting.java
@@ -0,0 +1,5 @@
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello Welcome to the Metropolia UAS Object Oriented Programming lecture.");
+    }
+}
