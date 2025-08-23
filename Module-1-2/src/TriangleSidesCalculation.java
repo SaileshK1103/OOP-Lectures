f06244202ee2149158f966ef643ffd82a69b8d69
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-08-23 20:03:55 +0300

    Fifth commit, created new module-1-2 and added two task with classes fahrenheit to celcius conversiona task completed and Traingle side calculation completed.

--- Module-1-2/src/TriangleSidesCalculation.java
+++ Module-1-2/src/TriangleSidesCalculation.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class TriangleSidesCalculation {
     public static void main(String[] args)
     {
-        int n = 0;
+        Scanner input = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter the length of first side of triangle ");
+        double a = input.nextDouble();
+
+        System.out.print("Enter the length of second side of triangle ");
+        double b = input.nextDouble();
+
+        // Calculate the other side of triangle
+        double hypotenuse = Math.sqrt(a*a + b*b);
+
+        // Output
+        System.out.printf("The hypotenuse is %.2f\n", hypotenuse);
+
+        input.close();
     }
 }
