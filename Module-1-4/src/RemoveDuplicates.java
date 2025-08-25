02a936ba36800e28ea17db387e2a455bffe6c354
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-08-25 20:52:47 +0300

    Twelveth commit, added new task with class RemoveDuplicates.

--- Module-1-4/src/RemoveDuplicates.java
+++ Module-1-4/src/RemoveDuplicates.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class RemoveDuplicates {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         // Input array size
         System.out.print("Enter the size of the array: ");
