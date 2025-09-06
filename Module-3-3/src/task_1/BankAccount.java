667c7cc61bc718b5acec5055c56c273d840096c9
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-09-06 13:13:05 +0300

    Thirty-fourth commit, new module module-3-3, added new package task_1, implemented bank account management system.

---
+++ Module-3-3/src/task_1/BankAccount.java
@@ -0,0 +1,62 @@
+package task_1;
+
+public class BankAccount {
+    private int accountNumber;
+    private double balance;
+
+    private static int totalAccounts =0;
+    private static int nextAccountNumber = 1; // auto increment
+
+    public BankAccount(double initialBalance){
+        this.balance = initialBalance;
+        this.accountNumber = nextAccountNumber++; // unique ID
+        totalAccounts++;
+    }
+
+    // Deposit money
+    public void deposit(double amount){
+        if(amount > 0){
+            balance += amount;
+            System.out.println("Deposited " + amount + " into account number " + accountNumber);
+        }
+        else{
+            System.out.println("Deposit amount must be positive");
+        }
+    }
+
+    // Withdraw money
+    public void withdraw(double amount){
+        if(amount > 0 && balance >= amount){
+            balance -= amount;
+            System.out.println("Withdraw " + amount + " from account " + accountNumber);
+        }
+        else{
+            System.out.println("Insufficient balance or invalid amount");
+        }
+    }
+
+    //  Getters
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+    // Static method
+    public static int getTotalAccounts(){
+        return totalAccounts;
+    }
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount(1000);
+        BankAccount account2 = new BankAccount(2000);
+
+        account1.deposit(500);
+        account2.withdraw(800);
+
+        System.out.println("Account " + account1.getAccountNumber() + " balance " + account1.getBalance());
+        System.out.println("Account " + account2.getAccountNumber() + " balance " + account2.getBalance());
+
+        System.out.println("Total number of accounts " + BankAccount.getTotalAccounts());
+    }
+}
